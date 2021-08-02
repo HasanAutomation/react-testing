78ab6359163b804f98d07ce1413480dd89fa6288
Hasan Ali <hasan.ali@lacritz.com>
2021-08-02 19:11:56 +0530

    Added second test

--- src/components/header/header.test.js
+++ src/components/header/header.test.js
@@ -6,6 +6,12 @@ describe('Header component', () => {
     it('Should render without errors', () => {
         const component = shallow(<Header />);
         const wrapper = component.find('.headerComponent');
-        expect(wrapper.length).toBe(2)
+        expect(wrapper.length).toBe(1)
+    });
+
+    it('Should render the logo properly', () => {
+        const component = shallow(<Header />);
+        const wrapper = component.find('.logoIMG');
+        expect(wrapper.length).toBe(1)
     })
 })
