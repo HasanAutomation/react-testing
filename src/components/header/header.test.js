5797a4f12df3a3e2527ab4fad1c681d614a557e7
Hasan Ali <hasan.ali@lacritz.com>
2021-08-02 19:15:55 +0530

    refactored the code

--- src/components/header/header.test.js
+++ src/components/header/header.test.js
@@ -2,15 +2,26 @@ import React from 'react';
 import { shallow } from 'enzyme'
 import Header from '.';
 
+const setUp = (props = {}) => {
+    const component = shallow(<Header {...props} />);
+    return component
+}
+
 describe('Header component', () => {
+
+    let component;
+
+    beforeEach(() => {
+        component = setUp();
+    })
+
+
     it('Should render without errors', () => {
-        const component = shallow(<Header />);
         const wrapper = component.find('.headerComponent');
         expect(wrapper.length).toBe(1)
     });
 
     it('Should render the logo properly', () => {
-        const component = shallow(<Header />);
         const wrapper = component.find('.logoIMG');
         expect(wrapper.length).toBe(1)
     })
